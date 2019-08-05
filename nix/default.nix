1876524107797fa8de0ecc4662dcb3c68d6b86cf
François Bobot <francois.bobot@cea.fr>
2019-08-05 23:57:06 +0200

    [Nix] try to give CLANG include dir

--- nix/default.nix
+++ nix/default.nix
@@ -5,6 +5,9 @@
 plugins.helpers.simple_plugin
    { inherit pkgs stdenv src opam2nix ocaml_version plugins;
      name = "frama-clang";
-     deps = [ pkgs.clang_4 pkgs.llvm_4 ];
+     deps = [ pkgs.llvmPackages_4.clang-unwrapped pkgs.llvm_4 pkgs.gnused ];
      opamPackages = [ "camlp4" ];
+     postPatch = ''
+     sed -i Makefile.config.in -e "s&@CLANG_INCDIR@&${pkgs.llvmPackages_4.clang-unwrapped}/include&"
+     '';
    }
