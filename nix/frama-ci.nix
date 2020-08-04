5d2e116486e25425d2999d56e4a096ba6a3047c3
Michele Alberti <michele.alberti@cea.fr>
2020-08-04 14:36:19 +0200

    Change in rev and ref.

--- nix/frama-ci.nix
+++ nix/frama-ci.nix
@@ -5,8 +5,8 @@ let
     src = builtins.fetchGit {
             "url" = "https://bobot:${password}@git.frama-c.com/frama-c/Frama-CI.git";
             "name" = "Frama-CI";
-            "rev" = "f65ce5dc3f2790667c7d62c23ef8fc04fbdd7ae5";
-            "ref" = "master";
+            "rev" = "ccd253f6f63812451cf6ce13a5b9e60c0f914829"
+            "ref" = "feature/frama-clang-ci";
     };
  in
  {
