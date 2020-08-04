112e0b5f126f0f4a67c526f98ae90b3430bc5ef6
Michele Alberti <michele.alberti@cea.fr>
2020-08-04 11:39:40 +0200

    CI file updates.

--- nix/default.nix
+++ nix/default.nix
@@ -5,7 +5,7 @@
 plugins.helpers.simple_plugin
    { inherit pkgs stdenv src opam2nix ocaml_version plugins;
      name = "frama-clang";
-     deps = [ pkgs.llvmPackages_7.clang-unwrapped pkgs.llvm_7 pkgs.gnused ];
-     opamPackages = [ "camlp4" ];
-     configure_options = "-with-clang-includedir=${pkgs.llvmPackages_7.clang-unwrapped}";
+     deps = [ pkgs.llvmPackages_9.clang-unwrapped pkgs.llvm_9 pkgs.gnused ];
+     opamPackages = [ "camlp5" ];
+     configure_options = "-with-clang-includedir=${pkgs.llvmPackages_9.clang-unwrapped}";
    }
