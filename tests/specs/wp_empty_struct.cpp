51dd85031136e207709d6dc32cc02d4b74384ef9
Virgile Prevosto <virgile.prevosto@m4x.org>
2020-08-05 16:38:19 +0200

    [tests] update options for tests involving WP

--- tests/specs/wp_empty_struct.cpp
+++ tests/specs/wp_empty_struct.cpp
@@ -1,9 +1,8 @@
 /* run.config
-OPT: -wp -wp-msg-key="no-time-info,no-step-info" -wp-cache none @CXX@
+OPT: -wp -wp-msg-key shell -wp-cache none @CXX@
 */
 class Point2
 {
     //@ ensures \result == *this;
     Point2& foo(void) { return *this; }
 };
-
