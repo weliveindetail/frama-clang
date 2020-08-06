ff30e7ae387da6981370e346d17e474eb48597c2
Michele Alberti <michele.alberti@cea.fr>
2020-08-06 10:44:29 +0200

    [Nix] Fetch and use unstable packages with RTTI fix.

--- nix/default.nix
+++ nix/default.nix
@@ -2,10 +2,19 @@
  { pkgs, stdenv, src ? ../., opam2nix,
    ocaml_version ? "ocamlPackages_latest.ocaml", plugins ? { } }:
 
+let
+     unstablePckgs = import (builtins.fetchGit {
+         # Descriptive name to make the store path easier to identify
+         name = "With-RTTI-fix";
+         url = "https://github.com/nixos/nixpkgs-channels/";
+         ref = "refs/heads/nixpkgs-unstable";
+         rev = "0f0b14258be090303c5013c2e29234040fa9766c";
+     }) {};
+in
 plugins.helpers.simple_plugin
    { inherit pkgs stdenv src opam2nix ocaml_version plugins;
      name = "frama-clang";
-     deps = [ pkgs.llvmPackages_9.clang-unwrapped pkgs.llvm_9 pkgs.gnused ];
+     deps = [ unstablePckgs.llvmPackages_9.clang-unwrapped unstablePckgs.llvm_9 pkgs.gnused ];
      opamPackages = [ "camlp5" ];
-     configure_options = "-with-clang-includedir=${pkgs.llvmPackages_9.clang-unwrapped}";
+     configure_options = "-with-clang-includedir=${unstablePckgs.llvmPackages_9.clang-unwrapped}";
    }
