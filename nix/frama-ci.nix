612e84361576ae61272541c1a32c9ad98643a856
Virgile Prevosto <virgile.prevosto@cea.fr>
2020-08-27 13:13:55 +0200

    Update nix/frama-ci.*

--- nix/frama-ci.nix
+++ nix/frama-ci.nix
@@ -5,8 +5,8 @@ let
     src = builtins.fetchGit {
             "url" = "https://bobot:${password}@git.frama-c.com/frama-c/Frama-CI.git";
             "name" = "Frama-CI";
-            "rev" = "58e51d3a0811dc7641781ec9784e4bd52e33a3a5";
-            "ref" = "feature/nix/add_ci_to_metacsl";
+            "rev" = "36e99854d59cda2c97d0b52f1f098f6beb3baf04";
+            "ref" = "master";
     };
  in
  {
