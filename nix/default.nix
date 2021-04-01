e5a36e8a780f3d755b64f5b94fb8ae58c9cc2364
Virgile Prevosto <virgile.prevosto@m4x.org>
2021-04-01 16:40:21 +0200

    [nix] extend compilation instructions for all llvm versions

--- nix/default.nix
+++ nix/default.nix
@@ -1,6 +1,8 @@
 # paramaterised derivation with dependencies injected (callPackage style)
  { pkgs, stdenv, src ? ../., opam2nix,
-   ocaml_version ? "ocamlPackages_latest.ocaml", plugins ? { } }:
+   ocaml_version ? "ocamlPackages_latest.ocaml", plugins ? { },
+   plugin_extend ? self: super: { }
+ }:
 
 let old_pkgs = pkgs; in
 
@@ -11,7 +13,7 @@ let frama_clang_build =
     llvm?pkgs.${"llvm_"+llvm_version},
     llvm_package?pkgs.${"llvmPackages_"+llvm_version} } :
 (plugins.helpers.simple_plugin
-   { inherit pkgs stdenv src opam2nix ocaml_version plugins;
+   { inherit pkgs stdenv src opam2nix ocaml_version plugins plugin_extend;
      name = "frama-clang-on-llvm-" + llvm_version;
      deps = [ llvm_package.clang-unwrapped llvm pkgs.gnused ];
      opamPackages = [ { name = "camlp5"; constraint="=7.14";} ];
