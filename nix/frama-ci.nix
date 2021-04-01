13d240d84523b0201b26cf0fc3b6ef60e65caa88
Virgile Prevosto <virgile.prevosto@cea.fr>
2021-04-01 14:48:20 +0000

    Update nix/frama-ci.*

--- nix/frama-ci.nix
+++ nix/frama-ci.nix
@@ -5,7 +5,7 @@ let
     src = builtins.fetchGit {
             "url" = "https://bobot:${password}@git.frama-c.com/frama-c/Frama-CI.git";
             "name" = "Frama-CI";
-            "rev" = "f9e0cd6c067f424cfe13abc7a8de49a8da43160f";
+            "rev" = "d42dbf3475a4a3f74449a2d64ffa122f81c18257";
             "ref" = "feature/pure-local-launch";
     };
  in
