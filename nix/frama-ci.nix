3af8684737fa3bc64c575d6ef4a75ded4571d2e7
Basile Desloges <basile.desloges@cea.fr>
2021-04-09 09:07:23 +0000

    Update nix/frama-ci.*

--- nix/frama-ci.nix
+++ nix/frama-ci.nix
@@ -5,8 +5,8 @@ let
     src = builtins.fetchGit {
             "url" = "https://bobot:${password}@git.frama-c.com/frama-c/Frama-CI.git";
             "name" = "Frama-CI";
-            "rev" = "e0b8d5ff621eb2953b848b2711e873e9ead25add";
-            "ref" = "e0b8d5ff621eb2953b848b2711e873e9ead25add";
+            "rev" = "22961f3356c3eb1279d817772b4f19c352d055a7";
+            "ref" = "master";
     };
  in
  {
