b456edd66edcada4a3cdacb598cca5ba0a127d60
Virgile Prevosto <virgile.prevosto@m4x.org>
2021-05-04 16:59:42 +0200

    [nix] update to new interface for plug-in derivation

--- nix/default.nix
+++ nix/default.nix
@@ -1,6 +1,6 @@
 # paramaterised derivation with dependencies injected (callPackage style)
  { pkgs, stdenv, src ? ../., opam2nix,
-   ocaml_version ? "ocamlPackages_latest.ocaml", plugins ? { },
+   ocaml ? "ocamlPackages_latest.ocaml", plugins ? { },
    plugin_extend ? self: super: { }
  }:
 
@@ -13,7 +13,7 @@ let frama_clang_build =
     llvm?pkgs.${"llvm_"+llvm_version},
     llvm_package?pkgs.${"llvmPackages_"+llvm_version} } :
 (plugins.helpers.simple_plugin
-   { inherit pkgs stdenv src opam2nix ocaml_version plugins plugin_extend;
+   { inherit pkgs stdenv src opam2nix ocaml plugins plugin_extend;
      name = "frama-clang-on-llvm-" + llvm_version;
      deps = [ llvm_package.clang-unwrapped llvm pkgs.gnused ];
      opamPackages = [ "camlp5=7.14" ];
