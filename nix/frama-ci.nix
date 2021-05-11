68aba337ed9ca1ba645e14e7dfd193b9dda095ca
Virgile Prevosto <virgile.prevosto@cea.fr>
2021-05-11 09:32:31 +0000

    Update nix/frama-ci.*

--- nix/frama-ci.nix
+++ nix/frama-ci.nix
@@ -5,7 +5,7 @@ let
     src = builtins.fetchGit {
             "url" = "https://bobot:${password}@git.frama-c.com/frama-c/Frama-CI.git";
             "name" = "Frama-CI";
-            "rev" = "9a16e1f8c817fcfe7434a8c2f591306ceab0b60e";
+            "rev" = "e94467e207414af4299b8597fbcfa127e5028926";
             "ref" = "feature/upgrade-opam2nix";
     };
  in
